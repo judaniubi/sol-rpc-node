3d923a9036d6161f04819052052dfda28c438b57
judaniubi <38403215+judaniubi@users.noreply.github.com>
2024-07-28 21:38:23 +0800

    Update mount-disk.sh

--- hack/mount-disk.sh
+++ hack/mount-disk.sh
@@ -10,7 +10,7 @@ sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
 
 sudo mkdir -p /mnt/disks/solana
 
-sudo mount -o discard,defaults /dev/sdb /mnt/disks/solana
+sudo mount -o discard,defaults /dev/vdb /mnt/disks/solana
 
 sudo chmod a+w /mnt/disks/solana
 
