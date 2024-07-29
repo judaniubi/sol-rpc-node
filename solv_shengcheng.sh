e83fba8d47b04f61772262b63e12cd57b74bde9c
judaniubi <38403215+judaniubi@users.noreply.github.com>
2024-07-30 01:29:27 +0800

    Create solv_shengcheng.sh

---
+++ solv_shengcheng.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+exec solana-validator \
+--identity /home/solv/mainnet-validator-keypair.json \
+--log /home/solv/solana-validator.log \
+--accounts /mnt/accounts \
+--ledger /mnt/ledger \
+--entrypoint entrypoint.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
+--known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
+--known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
+--known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
+--known-validator C1ocKDYMCm2ooWptMMnpd5VEB2Nx4UMJgRuYofysyzcA \
+--known-validator GwHH8ciFhR8vejWCqmg8FWZUCNtubPY2esALvy5tBvji \
+--known-validator 6WgdYhhGE53WrZ7ywJA15hBVkw7CRbQ8yDBBTwmBtAHN \
+--known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
+--expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
+--only-known-rpc \
+--full-rpc-api \
+--no-voting \
+--private-rpc \
+--dynamic-port-range 8000-8020 \
+--rpc-bind-address 0.0.0.0 \
+--gossip-port 8001 \
+--rpc-port 8899 \
+--account-index program-id \
+--wal-recovery-mode skip_any_corrupted_record \
+--use-snapshot-archives-at-startup when-newest \
+--limit-ledger-size 50000000 \
