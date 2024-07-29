bfb14d9ca60baf510857d4f5dc05b101e6500f32
judaniubi <38403215+judaniubi@users.noreply.github.com>
2024-07-29 18:55:46 +0800

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
-set -e
-set -u
-export RUST_LOG=error
-solana-validator \
-        --ledger /var/solana/data/ledger \
-        --accounts /var/solana/accounts \
-        --identity /var/solana/data/config/validator-keypair.json \
-        --known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
-        --known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
-        --known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
-        --known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
-        --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
-        --entrypoint entrypoint.mainnet-beta.solana.com:8001 \
-        --entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
-        --entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
-        --entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
-        --entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
-        --rpc-port 8899 \
-        --dynamic-port-range 8000-8020 \
-        --wal-recovery-mode skip_any_corrupted_record \
-        --init-complete-file /var/solana/data/init-completed \
-        --limit-ledger-size 50000000 \
-        --log - \
-        --only-known-rpc \
-        --full-rpc-api \
-        --no-voting \
-        --enable-rpc-transaction-history \
-        --enable-cpi-and-log-storage
+exec solana-validator \
+    --ledger /var/solana/data/ledger \
+    --accounts /var/solana/accounts \
+    --identity /var/solana/data/config/validator-keypair.json \
+    --known-validator 5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on \
+    --known-validator dDzy5SR3AXdYWVqbDEkVFdvSPCtS9ihF5kJkHCtXoFs \
+    --known-validator eoKpUABi59aT4rR9HGS3LcMecfut9x7zJyodWWP43YQ \
+    --known-validator 7XSY3MrYnK8vq693Rju17bbPkCN3Z7KvvfvJx4kdrsSY \
+    --known-validator Ft5fbkqNa76vnsjYNwjDZUXoTWpP7VYm3mtsaQckQADN \
+    --known-validator 9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv \
+    --only-known-rpc \
+    --full-rpc-api \
+    --no-voting \
+    --log /home/sol/solana-rpc.log \
+    --rpc-port 8899 \
+    --rpc-bind-address 0.0.0.0 \
+    --private-rpc \
+    --dynamic-port-range 8000-8020 \
+    --entrypoint entrypoint.testnet.solana.com:8001 \
+    --entrypoint entrypoint2.testnet.solana.com:8001 \
+    --entrypoint entrypoint3.testnet.solana.com:8001 \
+    --expected-genesis-hash 4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY \
+    --wal-recovery-mode skip_any_corrupted_record \
+    --limit-ledger-size
